5881abf4955efa57763265594e8b6990ac349a3a
h-miyamo <h-miyamo@example.com>
2014-05-07 09:11:06 +0000

    modifid ruby2.1 install

---
+++ site-cookbooks/pkg-mng/recipes/default.rb
@@ -0,0 +1,31 @@
+#
+# Cookbook Name:: pkg-mng
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+# Need to update package in order to install the Ruby2.1 
+# After the update completes, installation of ruby will start.
+
+case node['platform']
+when 'debian','ubuntu'
+  execute 'dpkg --configure -a' do
+    action :run
+  end
+  execute 'apt-get update' do
+    action :run
+  end
+  execute 'apt-get install -y ruby ruby-dev rubygems libicu-dev libssl-dev libopenssl-ruby libxslt-dev libxml2-dev monit' do
+    action :run
+  end
+  execute 'apt-get dist-upgrade -y' do
+    action :run
+  end
+
+when 'centos','redhat','fedora','amazon'
+  # TODO 
+end  
+
+#include_recipe 'ruby'
