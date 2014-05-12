20b8fc3376954c122820a22893f5bedf1d873ebb
h-miyamo <h-miyamo@example.com>
2014-05-12 08:50:44 +0000

    add data_bags

--- site-cookbooks/chef-td-agent/attributes/default.rb
+++ site-cookbooks/chef-td-agent/attributes/default.rb
@@ -1,4 +1,7 @@
-default[:td_agent][:api_key] = ''
+# get data bag data
+keys = Chef::EncryptedDataBagItem.load("api-key", "api-keys")
+
+default[:td_agent][:api_key] = keys["treasure-data"]
 
 default[:td_agent][:plugins] = []
 
